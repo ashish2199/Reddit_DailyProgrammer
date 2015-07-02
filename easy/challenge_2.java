55822b43962ad5a6beba6d5848d2bb4e98e49205
ashish2199 <ashish2199@gmail.com>
2015-07-02 10:05:16 +0530

    Completed [easy]challenge #2

--- easy/challenge_2.java
+++ easy/challenge_2.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package easy;
-
+import java.util.Scanner;
 /**
  *
  * @author Ashish Padalkar
@@ -15,6 +15,35 @@ public class challenge_2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Physics Force Calculator");
+        System.out.println("What do you want to calculate : F-Force M-Mass A-Acceleration");
+        String option = sc.next();
+        if(option.equals("F")){
+            System.out.println("Enter the value for Mass:");
+            int m = sc.nextInt();
+            System.out.println("Enter the value for Acceleration:");
+            int a = sc.nextInt();
+            int f=m*a;
+            System.out.printf("The Value of force is %d N\n",f);
+        }
+        if(option.equals("M")){
+            System.out.println("Enter the value for Force:");
+            int f = sc.nextInt();
+            System.out.println("Enter the value for Acceleration:");
+            int a = sc.nextInt();
+            double m=((double)f)/a;
+            System.out.printf("The Value of mass is %f Kg\n",m);
+        }
+        if(option.equals("A")){
+            System.out.println("Enter the value for Force:");
+            int f = sc.nextInt();
+            System.out.println("Enter the value for Mass:");
+            int m = sc.nextInt();
+            double a=((double)f)/m;
+            System.out.printf("The Value of Acceleration is %f m/s^-2\n",a);
+        }
+        
         // TODO code application logic here
     }
     
